9ba3870aa80d3a258a028dc79c63c5c5e7f8897a
Silsebas <jordancubillo@gmail.com>
2025-11-25 20:00:34 -0600

    Se crea la relación reserva pertenece a un pasajero y a un ride.

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Los atributos que son asignables en masa.
+     */
+    protected $fillable = [
+        'pasajero_id',
+        'ride_id',
+        'espacios_solicitados',
+        'estado',
+    ];
+
+    // --- RELACIONES ---
+
+    /**
+     * Obtiene el usuario (pasajero) que hizo la reserva.
+     */
+    public function pasajero()
+    {
+        return $this->belongsTo(Usuario::class, 'pasajero_id');
+    }
+
+    /**
+     * Obtiene el ride al que pertenece la reserva.
+     */
+    public function ride()
+    {
+        return $this->belongsTo(Ride::class, 'ride_id');
+    }
 }
